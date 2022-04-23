70838ec1bd5c02f87e9af1e44b9e16e8662db9b8
Atracytr <atracytr@gmail.com>
2022-04-22 22:25:36 -0700

    update files

--- app/src/main/java/com/example/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/example/cryptotracker/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.cryptotracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import fragments.HomeFragment
 import fragments.NewsFragment
+import okhttp3.Headers
+private  const val TAG = "MainActivity"
+private const val  CRYPTO_MARKET_URL = "https://api.nomics.com/v1/currencies/ticker?key=d489d947fe979cf49117677d2e9946450f100441&interval=1d,30d&per-page=100&page=2"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,5 +46,24 @@ class MainActivity : AppCompatActivity() {
         // Set default selection
         findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_home
 
+
+        val client = AsyncHttpClient()
+        client.get(CRYPTO_MARKET_URL, object: JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.i(TAG, "onFailure $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                Log.i(TAG, "onSuccess: JSON data $json")
+                // val cryptoJsonArray = json.jsonObject.getJSONArray("")
+
+            }
+
+        })
     }
 }
